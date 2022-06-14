05a85c43da0f0b361d14f83bd9761331e30613c0
Zhanglin <jason.lin.wu@163.com>
2022-06-14 14:44:13 +0800

    Add example for generating IR for "return" statement

---
+++ 5.Return/HelloReturn.cpp
@@ -0,0 +1,44 @@
+// HelloReturn.cpp
+
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/GlobalVariable.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Verifier.h"
+
+using namespace llvm;
+
+int main(int argc, char* argv[])
+{
+    LLVMContext context;
+    IRBuilder<> builder(context);
+
+    // Create a module
+    Module* module = new Module("HelloModule", context);
+
+    // Add a global variable
+    module->getOrInsertGlobal("helloGlobalVariable", builder.getInt32Ty());
+    GlobalVariable* globalVariable = module->getNamedGlobal("helloGlobalVariable");
+    globalVariable->setLinkage(GlobalValue::CommonLinkage);
+    globalVariable->setAlignment(MaybeAlign(4));
+
+    // Add a function
+    FunctionType* functionType = FunctionType::get(builder.getVoidTy(), false);
+    Function* function = Function::Create(functionType, GlobalValue::ExternalLinkage, "HelloFunction", module);
+
+    // Create a block
+    BasicBlock* block = BasicBlock::Create(context, "entry", function);
+    builder.SetInsertPoint(block);
+
+    // Add a return
+    ConstantInt* zero = builder.getInt32(0);
+    builder.CreateRet(zero);
+
+    // Print the IR
+    verifyFunction(*function);
+    module->print(outs(), nullptr);
+
+    return 0;
+}
---
+++ 5.Return/README.md
@@ -0,0 +1,33 @@
+# LLVM Function IR
+
+## Build and Run
+
+#### Commands
+
+```
+# Format the source code
+clang-format -i *.cpp
+
+# Set up C++ standard library and header path
+export SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+
+# Compile
+clang++ -w -o HelloReturn `llvm-config --cxxflags --ldflags --system-libs --libs core` HelloReturn.cpp
+
+# Run
+./HelloReturn
+```
+
+#### Output
+
+```
+; ModuleID = 'HelloModule'
+source_filename = "HelloModule"
+
+@helloGlobalVariable = common global i32, align 4
+
+define void @HelloFunction() {
+entry:
+  ret i32 0
+}
+```
